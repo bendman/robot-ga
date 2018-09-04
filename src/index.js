502eb676b565d2aa390efc4de7c79cc118239cf3
bendman <duncan.ben@gmail.com>
2018-09-03 19:13:27 -0700

    adding generation fitness output in table

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-class Model {
+class Model extends EventTarget {
 
-  constructor() {
+  /**
+   * Setup the Model, binding callbacks and setting parameters
+   * @param {HTMLElement} tableElement output data table
+   */
+  constructor(tableElement) {
+    super();
     this.onGenerationReceived = this.onGenerationReceived.bind(this);
 
+    this.tableElement = tableElement;
     this.worker = new Worker('./dist/worker.js');
   }
 
@@ -10,23 +16,74 @@ class Model {
     this.worker.addEventListener('message', this.onGenerationReceived);
     this.worker.postMessage({ action: 'start' });
     this.isRunning = true;
+    this.dispatchEvent(new Event('start'));
   }
 
   stop() {
     this.worker.removeEventListener('message', this.onGenerationReceived);
     this.worker.postMessage({ action: 'stop' });
     this.isRunning = false;
+    this.dispatchEvent(new Event('stop'));
   }
 
   onGenerationReceived(message) {
-    const incomingBest = event.data.bestIndividual;
+    const individual = message.data.bestIndividual;
+    const genMean = message.data.meanFitness;
 
-    if (!this.best || incomingBest.fitness > this.best.fitness) {
-      this.best = incomingBest;
+    this.dispatchEvent(new CustomEvent('generation', { detail: {
+      newBest: !this.best || individual.fitness > this.best.fitness,
+      runningBest: this.best,
+      best: individual,
+      mean: genMean,
+    }}));
+
+    if (!this.best || individual.fitness > this.best.fitness) {
+      this.best = individual;
+      this.dispatchEvent(new CustomEvent('best', { detail: {
+        individual,
+        genMean,
+      }}));
     }
   }
 
 }
 
-const model = new Model();
+const model = new Model(document.getElementById('output_table'));
+
+//
+// Toggling GA
+//
+const toggler = document.getElementById('toggle_button');
+
+toggler.addEventListener('click', () => {
+  if (model.isRunning) {
+    model.stop();
+    toggler.innerHTML = 'Start Evolution';
+  } else {
+    model.start();
+    toggler.innerHTML = 'Stop Evolution';
+  }
+});
+
+// 
+// GA Results
+//
+const output = document.getElementById('output_table');
+
+model.addEventListener('generation', (event) => {
+  const { best, mean, newBest } = event.detail;
+  console.log('new best!', best, best.fitness, mean);
+  const newRow = document.createElement('tr');
+  if (newBest) {
+    newRow.classList.add('new-best');
+  }
+  newRow.innerHTML = `
+    <td>${best.generation}</td>
+    <td>${mean.toFixed(2)}</td>
+    <td>${best.fitness.toFixed(2)}</td>
+  `;
+
+  output.appendChild(newRow);
+});
+
 window.model = model;
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  mode: process.env.WEBPACK_SERVE ? 'development' : 'production',
   entry: {
     main: './src/index.js',
     worker: './src/worker.js',
